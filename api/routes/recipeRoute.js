361acd6edcbe18b72864b5f360757c9bf49368f7
MarilyneRios <rios.marilyne@gmail.com>
2024-09-10 17:58:36 +0200

    suppression d  une recette logique api ok

--- api/routes/recipeRoute.js
+++ api/routes/recipeRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { createRecipe, displayAllRecipes,displayOneRecipe, updateRecipe } from '../controllers/recipeController.js';
+import { createRecipe, displayAllRecipes,displayOneRecipe, updateRecipe, deleteRecipe } from '../controllers/recipeController.js';
 
 import { verifyToken } from '../utils/verifyUser.js';
 
@@ -15,4 +15,8 @@ router.post('/', verifyToken, createRecipe); //POST: http://localhost:3000/api/r
 router.get ('/displayAllRecipes', displayAllRecipes) //GET: http://localhost:3000/api/recipes/displayAllRecipes 
 router.get ('/displayOneRecipe/:id',  verifyToken, displayOneRecipe) //GET: http://localhost:3000/api/recipes/displayOneRecipe/66cc83a167d91d80563f7b25
 router.post ('/updateRecipe/:id', verifyToken, updateRecipe) // POST: http://localhost:3000/api/recipes/updateRecipe/66def36e2f915a6f29010b79
+router.delete ('/deleteRecipe/:id', verifyToken, deleteRecipe) // DELETE: http://localhost:3000/api/recipes/deleteRecipe/66def36e2f915a6f29010b79 
+
+
+
 export default router;
