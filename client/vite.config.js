7b76bb917c602504d7ee83aa444aa13834edcc1a
MarilyneRios <rios.marilyne@gmail.com>
2025-05-11 14:45:34 +0200

    reduction image asset

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +13,10 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({ open: true })
+  ],
   optimizeDeps: {
     exclude: ['chunk-m324agam.js']
   },
